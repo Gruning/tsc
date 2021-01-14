7dbf19f4a213105ba4b4b0d8ba964f59f918283e
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-01-14 19:20:56 -0300

    custom parameter type

--- foo.ts
+++ foo.ts
@@ -1,5 +1,5 @@
-function foo(bar?: number[]) {
+function foo(bar?: number[],custom:'car'|'bus'| number ) {
     return "hello " + bar;
 }
-let baz = "ABC";
-console.log(foo(baz));
+let baz = [1,2,3];
+console.log(foo(baz,'car'));
