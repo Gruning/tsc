8107c219c5a2fee1e9dcb90bd3c87408dd9bebc9
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-01-18 21:06:11 -0300

    cap4-typeAnnotations

--- foo.js
+++ foo.js
@@ -1,5 +1,5 @@
-function foo(bar) {
+function foo(bar, custom, wild, wild2) {
     return "hello " + bar;
 }
-var baz = "ABC";
-console.log(foo(baz));
+var baz = [1, 2, 3];
+console.log(foo(baz, 'car', true, 'abc'));
--- foo.ts
+++ foo.ts
@@ -1,4 +1,4 @@
-function foo(bar?: number[],custom:'car'|'bus'| number,wild:any , wild2:any ) {
+function foo(bar: number[],custom:'car'|'bus'| number,wild:any , wild2:any ) {
     return "hello " + bar;
 }
 let baz = [1,2,3];
